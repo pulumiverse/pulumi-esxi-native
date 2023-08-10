cb32ab6eec2d916eb7388c559643d461a4123607
edmondshtogu <edmond.shtogu@deliveryhero.com>
2023-08-10 22:41:36 +0200

    chore: fixing test 04

---
+++ examples/04_ovf_source_virtual_machine/nodejs/index.ts
@@ -0,0 +1,57 @@
+import {VirtualMachine} from "@pulumiverse/esxi-native";
+
+export = async () => {
+    // https://github.com/tsugliani/packer-alpine
+    const ovfSource = "https://cloud.tsugliani.fr/ova/alpine-3.15.6.ova";
+
+    const vm = new VirtualMachine("vm-test", {
+        diskStore: "nvme-ssd-datastore",
+        os: "otherlinux",
+        bootDiskSize: 8,
+        numVCpus: 1,
+        memSize: 512,
+        shutdownTimeout: 5,
+        power: "on",
+        networkInterfaces: [
+            {
+                virtualNetwork: "default"
+            }
+        ],
+        // Specify ovf_properties specific to the source ovf/ova.
+        // Use ovftool <filename>.ova to get details of which ovf_properties are available.
+        ovfProperties: [
+            {
+                key: "guestinfo.hostname",
+                value: "pulumi"
+            },
+            {
+                key: "guestinfo.gateway",
+                value: "192.168.20.1"
+            },
+            {
+                key: "guestinfo.dns",
+                value: "1.1.1.1"
+            },
+            {
+                key: "guestinfo.password",
+                value: "secret"
+            },
+            // guestinfo.ipaddress "192.168.0.11"
+            // guestinfo.netprefix "24"
+            // guestinfo.gateway "192.168.0.1"
+            // guestinfo.dns "1.1.1.1"
+            // guestinfo.domain "zpod.io"
+            // guestinfo.password "VMware1!" (default password)
+            // guestinfo.sshkey "ssh-rsa AAAA...[concatened].... tsugliani@zpod"
+        ],
+        // Specify an ovf file to use as a source.
+        ovfSource: ovfSource,
+        virtualHWVer: 14
+    });
+
+    return {
+        "id": vm.id,
+        "name": vm.name,
+        "os": vm.os,
+    };
+}
