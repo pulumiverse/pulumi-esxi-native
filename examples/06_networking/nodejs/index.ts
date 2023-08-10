848dd4a6a1b9195355e2b00938f5737d0593a1ee
edmondshtogu <edmond.shtogu@deliveryhero.com>
2023-08-11 01:47:46 +0200

    chore: Improved tests

--- examples/06_networking/nodejs/index.ts
+++ examples/06_networking/nodejs/index.ts
@@ -1,29 +1,27 @@
-import {PortGroup, VirtualMachine, VirtualSwitch} from "@pulumiverse/esxi-native";
+import {PortGroup, VirtualSwitch} from "@pulumiverse/esxi-native";
 
-// ESXI vSwitch resource
-// Example vSwitch with defaults.
-// Uncomment the uplink block to connect this vSwitch to your nic.
-const vSwitch = new VirtualSwitch("my-v-switch", {
-    //name: "My vSwitch",
-    // uplinks: [
-    //     {
-    //         name: "vmnic0"
-    //     }
-    // ]
-});
+export = async () => {
+    // ESXI vSwitch resource
+    // Example vSwitch with defaults.
+    // Uncomment the uplink block to connect this vSwitch to your nic.
+    const vSwitch = new VirtualSwitch("my-v-switch", {
+        //name: "My vSwitch",
+        // uplinks: [
+        //     {
+        //         name: "vmnic0"
+        //     }
+        // ]
+    });
 
-// ESXI Port Group resource
-// Example port group with default, connecting to the above vSwitch.
-const portGroup = new PortGroup("my-port-group", {
-    //name: "My Port Group",
-    vSwitch: vSwitch.name,
-});
+    // ESXI Port Group resource
+    // Example port group with default, connecting to the above vSwitch.
+    const portGroup = new PortGroup("my-port-group", {
+        //name: "My Port Group",
+        vSwitch: vSwitch.name,
+    });
 
-new VirtualMachine("vm-test", {
-    diskStore: "nvme-ssd-datastore",
-    networkInterfaces: [
-        {
-            virtualNetwork: portGroup.name
-        }
-    ]
-});
+    return {
+        "vSwitch": vSwitch.name,
+        "portGroup": portGroup.name,
+    };
+}
--- examples/07_networking_cloud_init/nodejs/index.ts
+++ examples/07_networking_cloud_init/nodejs/index.ts
@@ -1,89 +1,86 @@
-import { gzip } from 'zlib';
-import {Output} from "@pulumi/pulumi";
 import {PortGroup, VirtualSwitch, VirtualMachine} from "@pulumiverse/esxi-native";
 
-function base64gzip(input: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        gzip(input, (error, compressedBuffer) => {
-            if (error) {
-                reject(error);
-            } else {
-                const base64Compressed = compressedBuffer.toString('base64');
-                resolve(base64Compressed);
+export = async () => {
+    // https://github.com/tsugliani/packer-alpine
+    const ovfSource = "https://cloud.tsugliani.fr/ova/alpine-3.15.6.ova";
+    const ipAddress = "10.10.10.10/24";
+    const gateway = "10.10.10.1";
+    const nameserver = "8.8.8.8";
+
+
+    // ESXI vSwitch resource
+    // Example vSwitch with defaults.
+    // Uncomment the uplink block to connect this vSwitch to your nic.
+    const vSwitch = new VirtualSwitch("my-v-switch", {
+        // uplinks: [
+        //     {
+        //         name: "vmnic0"
+        //     }
+        // ]
+    });
+
+    // ESXI Port Group resource
+    // Example port group with default, connecting to the above vSwitch.
+    const portGroup = new PortGroup("my-port-group", {
+        vSwitch: vSwitch.name,
+        // vSwitch: "vSwitch0", // using default vlan
+    });
+
+    const vm = new VirtualMachine("vm-test", {
+        diskStore: "nvme-ssd-datastore",
+        os: "otherlinux",
+        bootDiskSize: 8,
+        numVCpus: 1,
+        memSize: 512,
+        shutdownTimeout: 5,
+        power: "on",
+        networkInterfaces: [
+            {
+                virtualNetwork: portGroup.name
             }
-        });
+        ],
+        // Specify ovf_properties specific to the source ovf/ova.
+        // Use ovftool <filename>.ova to get details of which ovf_properties are available.
+        ovfProperties: [
+            {
+                key: "guestinfo.hostname",
+                value: "pulumi"
+            },
+            {
+                key: "guestinfo.gateway",
+                value: gateway
+            },
+            // {
+            //     key: "guestinfo.netprefix",
+            //     value: "20"
+            // },
+            {
+                key: "guestinfo.ipaddress",
+                value: ipAddress
+            },
+            {
+                key: "guestinfo.dns",
+                value: nameserver
+            },
+            {
+                key: "guestinfo.domain",
+                value: "alpine.local"
+            },
+            {
+                key: "guestinfo.password",
+                value: "secret"
+            },
+        ],
+        // Specify an ovf file to use as a source.
+        ovfSource: ovfSource,
+        virtualHWVer: 14
     });
+
+    return {
+        "id": vm.id,
+        "ip": vm.ipAddress,
+        "name": vm.name,
+        "os": vm.os,
+        "vlan": portGroup.vlan
+    };
 }
-
-let output: Output<string>;
-
-const ipAddress = "10.10.10.10/24";
-const gateway = "10.10.10.1";
-const nameserver = "8.8.8.8";
-
-const metadata = `
-network:
-    version: 2
-    ethernets:
-        ens192:
-            dhcp4: false
-            addresses:
-                - ${ipAddress}
-            gateway4: ${gateway}
-            nameservers:
-                addresses:
-                    - ${nameserver}
-`;
-
-
-// ESXI vSwitch resource
-// Example vSwitch with defaults.
-// Uncomment the uplink block to connect this vSwitch to your nic.
-const vSwitch = new VirtualSwitch("my-v-switch", {
-    // uplinks: [
-    //     {
-    //         name: "vmnic0"
-    //     }
-    // ]
-});
-
-// ESXI Port Group resource
-// Example port group with default, connecting to the above vSwitch.
-const portGroup = new PortGroup("my-port-group", {
-    vSwitch: vSwitch.name,
-    // vSwitch: "vSwitch0", // using default vlan
-});
-
-base64gzip(metadata)
-    .then((base64Compressed) => {
-        // This VM is "bare-metal". It will be powered on by the
-        // Pulumi, but it will not boot to any OS. It will however attempt
-        // to network boot on the port group configured above.
-
-        const vm = new VirtualMachine("vm-test", {
-            diskStore: "nvme-ssd-datastore",
-            networkInterfaces: [
-                {
-                    virtualNetwork: portGroup.name
-                }
-            ],
-            info: [
-                {
-                    key: "metadata.encoding",
-                    value: "gzip+base64"
-                },
-                {
-                    key: "metadata",
-                    value: base64Compressed
-                },
-            ],
-            power: "on"
-        });
-
-        output = vm.ipAddress;
-    })
-    .catch((error) => {
-        console.error('Error compressing:', error);
-    });
-
-
